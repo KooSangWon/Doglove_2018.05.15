76609d2c557886f3d6f1a09bfd1a0f82e145f00b
KooSangWon <gsw0931@gmail.com>
2018-05-15 09:50:09 +0900

    doglove_2018.05.15

---
+++ commit_push.bat
@@ -0,0 +1,3 @@
+git add --all
+git commit -m "doglove_2018.05.15"
+git push -u origin master
---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <section class="section">
+    <div class="content">
+<h1 class="title">개사랑</h1>
+<h2 class="subtitle">개를 사랑합니다.</h2>
+
+    </div>
+    
+  </section>
+</template>
+
+<script>
+
+</script>
